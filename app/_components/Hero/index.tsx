24c751d1bc3abc620763c792ded228b3c6f60002
ktc <ktc@503-06.local>
2025-02-01 14:33:22 +0900

    ”コミット”

--- app/_components/Hero/index.tsx
+++ app/_components/Hero/index.tsx
@@ -15,7 +15,7 @@ export default function Hero({ title, sub }: Props) {
         </div>
         <Image
           className={styles.bgimg}
-          src="/img-mv.jpg"
+          src="/photo0000-1252.webp"
           alt=""
           width={4000}
           height={1200}
