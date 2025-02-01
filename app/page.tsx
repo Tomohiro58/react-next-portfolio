64002a524c18cafae7b9e9e9a5015d95823885bb
ktc <ktc@503-06.local>
2025-02-01 15:45:29 +0900

    ”コミット”

--- app/page.tsx
+++ app/page.tsx
@@ -18,7 +18,7 @@ export default async function Home(){
       </div>
     <Image
       className={styles.bgimg}
-      src="/写真.jpg"
+      src="/photo0000-1252.webp"
       alt=""
       width={4000}
       height={2000}
