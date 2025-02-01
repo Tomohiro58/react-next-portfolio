c312b0197f9a55babc0140ab4c76e6d39054dbb7
Tomohiro58 <tm0508hr@icloud.com>
2025-02-01 09:25:51 +0900

    コミット

--- app/_components/Footer/index.tsx
+++ app/_components/Footer/index.tsx
@@ -10,7 +10,7 @@ export default function Footer() {
               <Link href="/news">ニュース</Link>
             </li>
             <li className={styles.item}>
-              <Link href="/members">僕について</Link>
+              <Link href="/members">自分について</Link>
             </li>
             <li className={styles.item}>
               <Link href="/contact">お問い合わせ</Link>
